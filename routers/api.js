9d2c61898fc854c17e5b97cc427b15aa59359de2
riazahmedmahin <c201060@ugrad.iiuc.ac.bd>
2025-01-31 17:25:20 +0600

    commit

--- routers/api.js
+++ routers/api.js
@@ -1,15 +1,11 @@
 import express from "express";
 import { CreateUser, loginUser, ReadProfile } from '../services/controllers/UserController.js';
-import {
-  CreateParcel,
-    ListByParcel,
-    ParcelDetailsById,
-    UpdateParcel,
-    DeleteParcel,
-  } from '../services/controllers/ParcelController.js';
+import {CreateParcel,ListByParcel,ParcelDetailsById,UpdateParcel,DeleteParcel,} from '../services/controllers/ParcelController.js';
 import auth from "../services/middleware/auth.js";
+
 const router = express.Router();
-import * as UserControllers from "../services/controllers/UserController.js";
+
+
 // USER
 router.post('/signup', CreateUser);
 router.post('/login',loginUser); // Changed from GET to POST
